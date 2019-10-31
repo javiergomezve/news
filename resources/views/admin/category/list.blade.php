02c4ae54feda0c2715831a3c8c1cd5f5b10c1bbd
Javier Gómez <javiergomezve@gmail.com>
2019-10-31 17:16:21 -0500

    [fix] Assets url

--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -2,7 +2,7 @@
   @section('content')
 
  
-    <link rel="stylesheet" href="{{ asset('public/admin/assets/css/lib/datatable/dataTables.bootstrap.min.css ') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/lib/datatable/dataTables.bootstrap.min.css ') }}">
     <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
     
  
@@ -104,22 +104,22 @@
             </div><!-- .animated -->
         </div><!-- .content -->
 
-    <script src="{{ asset('public/admin/assets/js/vendor/jquery-2.1.4.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/vendor/jquery-2.1.4.min.js ') }}"></script>
    
-    <script src="{{ asset('public/admin/assets/js/plugins.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/main.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/plugins.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/main.js ') }}"></script>
 
-<script src="{{ asset('public/admin/assets/js/lib/data-table/datatables.min.js') }}  "></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/dataTables.bootstrap.min.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/dataTables.buttons.min.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/buttons.bootstrap.min.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/jszip.min.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/pdfmake.min.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/vfs_fonts.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/buttons.html5.min.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/buttons.print.min.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/buttons.colVis.min.js ') }}"></script>
-    <script src="{{ asset('public/admin/assets/js/lib/data-table/datatables-init.js ') }}"></script>
+<script src="{{ asset('admin/assets/js/lib/data-table/datatables.min.js') }}  "></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/dataTables.bootstrap.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/dataTables.buttons.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/buttons.bootstrap.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/jszip.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/pdfmake.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/vfs_fonts.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/buttons.html5.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/buttons.print.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/buttons.colVis.min.js ') }}"></script>
+    <script src="{{ asset('admin/assets/js/lib/data-table/datatables-init.js ') }}"></script>
 
 
     <script type="text/javascript">
--- resources/views/admin/layout/top.blade.php
+++ resources/views/admin/layout/top.blade.php
@@ -5,20 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="apple-icon.png">
-    <link rel="shortcut icon" href="{{ asset('public/others') }}/{{ $shareData['favicon'] }}">
+    <link rel="shortcut icon" href="{{ asset('others') }}/{{ $shareData['favicon'] }}">
 
-    <link rel="stylesheet" href="{{ asset('public/admin/assets/css/normalize.css') }} ">
-    <link rel="stylesheet" href="{{ asset('public/admin/assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin/assets/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin/assets/css/themify-icons.css' ) }}">
-    <link rel="stylesheet" href="{{ asset('public/admin/assets/css/flag-icon.min.css ') }}">
-    <link rel="stylesheet" href="{{ asset('public/admin/assets/css/cs-skin-elastic.css' ) }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/normalize.css') }} ">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/themify-icons.css' ) }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/flag-icon.min.css ') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/cs-skin-elastic.css' ) }}">
     <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
-    <link rel="stylesheet" href="{{ asset('public/admin/assets/scss/style.css' ) }}">
-    <link href="{{ asset('public/admin/assets/css/lib/vector-map/jqvmap.min.css ') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('admin/assets/scss/style.css' ) }}">
+    <link href="{{ asset('admin/assets/css/lib/vector-map/jqvmap.min.css ') }}" rel="stylesheet">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-     <script src="{{asset('public/admin/assets/js/vendor/jquery-2.1.4.min.js')}}"></script>
-     <link rel="stylesheet" href="{{ asset('public/admin/assets/css/lib/chosen/chosen.min.css') }} ">
+     <script src="{{asset('admin/assets/js/vendor/jquery-2.1.4.min.js')}}"></script>
+     <link rel="stylesheet" href="{{ asset('admin/assets/css/lib/chosen/chosen.min.css') }} ">
 
     <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
